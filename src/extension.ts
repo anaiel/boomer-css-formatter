c17875fe4eccea8aad7fffa398f8fb721374a6f9
anaiel <an.leclabart@gmail.com>
2021-11-12 18:59:04 +0100

    :wrench: update configuration to make everything run smoothly

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,23 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "boomer-formatter" is now active!');
+  const disposable = vscode.commands.registerCommand(
+    "boomer-formatter.format",
+    () => {
+      const { activeTextEditor } = vscode.window;
+      if (!activeTextEditor || !activeTextEditor.document.languageId) return;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('boomer-formatter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from boomer-formatter!');
-	});
+      const { document } = activeTextEditor;
+      const firstLine = document.lineAt(0);
 
-	context.subscriptions.push(disposable);
+      if (firstLine.text !== "42") {
+        const edit = new vscode.WorkspaceEdit();
+        edit.insert(document.uri, firstLine.range.start, "42\n");
+
+        return vscode.workspace.applyEdit(edit);
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
